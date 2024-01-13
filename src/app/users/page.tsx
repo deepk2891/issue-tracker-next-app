7d05bc398b8b7236aac237580ee4b3987eea9671
Deep Kothari <deepk2891@gmail.com>
2024-01-13 18:16:57 +0530

    Data fetching from jsonplaceholder

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,7 +1,29 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 
-const UserPage = () => {
-	return <div>UserPage</div>
+//🟨Fetch Data on the Client
+//	UseState()  =>  Declare a state variable
+//	UseEffect() =>  Call the Backend, Get the Data, Put into State variable
+
+interface User {
+	id: number
+	name: string
+}
+
+const UserPage = async () => {
+	const res = await fetch("https://jsonplaceholder.typicode.com/users")
+	const users: User[] = await res.json()
+
+	return (
+		<>
+			<div>UserPage</div>
+			<h1>Users</h1>
+			<ul>
+				{users.map((user) => (
+					<li key={user.id}>{user.name}</li>
+				))}
+			</ul>
+		</>
+	)
 }
 
 export default UserPage
