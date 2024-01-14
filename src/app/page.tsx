b94b7ce9566dd8f768b433dd3c26eafeb8fc238c
Deep Kothari <deepk2891@gmail.com>
2024-01-14 17:59:54 +0530

    Build the navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import Image from "next/image"
-import Link from "next/link"
 
 export default function Home() {
 	return (
 		<main>
-			<h1>Hello world</h1>
-			<Link href="/users">users</Link>
+			<h1>home</h1>
 		</main>
 	)
 }
